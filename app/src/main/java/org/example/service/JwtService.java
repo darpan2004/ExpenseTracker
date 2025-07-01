c7dab7eb69762babd2e67e8cc1950490f2b4075c
Darpan Khatri <darpankhatri2004@gmail.com>
2025-07-01 16:07:08 +0530

    Update JWT token class

--- app/src/main/java/org/example/service/JwtService.java
+++ app/src/main/java/org/example/service/JwtService.java
@@ -1,5 +1,38 @@
 package org.example.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
-    public  static final String SECRET=""
+    // Use a properly Base64-encoded secret key
+    public static final String SECRET = "dGhpc0lzQVNlY3JldEtleU5lZWRzVG9CZUJhc2U2NEVuY29kZWQ=";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
